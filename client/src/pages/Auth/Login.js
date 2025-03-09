b6f5a61ae01059dbc82dd134853663b765e77355
linxingting <timlinxingting@gmail.com>
2025-03-09 21:57:39 +0800

    cleanup the comments written during dev

--- client/src/pages/Auth/Login.js
+++ client/src/pages/Auth/Login.js
@@ -6,59 +6,23 @@ import toast from "react-hot-toast";
 import "../../styles/AuthStyles.css";
 import { useAuth } from "../../context/auth";
 
-// The user visits /login, and React renders Login.js.
-// The user enters their email and password.
-// Clicking "LOGIN":
-// Triggers handleSubmit().
-
-// Sends a POST request to /api/v1/auth/login.
-// If the login is successful:
-// Stores auth in localStorage.
-// Redirects to /dashboard or /.
-
-// If it fails, an error message is displayed.
-// If the user refreshes the page, the auth in localStorage restores the login state.
-
-
-// arrow function, equivalent to function Login() {
-//  xxx
-// }
 const Login = () => {
-  // define the states - Hook in React
-  // when states change, React will re-render the UI automatically
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
-  // useAuth() is a customized hook, to store the user + token globally
   const [auth, setAuth] = useAuth();
   
-  // for jumping between pages
   const navigate = useNavigate();
-  // to get the current page URL - location.state
   const location = useLocation();
   
   // form function
-
-  // What is e in handleSubmit(e)?
-  // the parameter e represents the event object that is automatically passed to the event handler when an event (like a form submission) occurs.
-  // e is an instance of SyntheticEvent in React.
-  // It contains information about the event (like type, target element, etc.).
-  // It is automatically provided by React when the function is triggered as an event handler.
   const handleSubmit = async (e) => {
-    // triggered when the user clicks lOGIN
-    // prevent default refreshing, otherwise the page will be reloaded, and the React state is lost
-    // we want to use axios to send requests from the FE, without refreshing
     e.preventDefault();
     try {
-      // send email + password to BE using the API URL (refer to server.js), which calls authRoute.js, then calls authController.js
-      // await will wait for the result
       const res = await axios.post("/api/v1/auth/login", {
         email,
         password,
       });
       if (res && res.data.success) {
-        // the success notification uses short-circuit to prevent type error
-        // but in this case not necessary as there is an if-statement before this
         toast.success(res.data && res.data.message, {
           duration: 5000,
           icon: "🙏",
@@ -67,15 +31,11 @@ const Login = () => {
             color: "white",
           },
         });
-        // update auth
-        // ... is used to keep other attributes, and only update what is specified
-        // otherwise other attributes will be lost
         setAuth({
             ...auth,
             user: res.data.user,
             token: res.data.token,
         });
-        // localStorage - to keep user online even if they refresh
         localStorage.setItem("auth", JSON.stringify(res.data));
         // go back to the current page if it exists, otherwise homepage
         navigate(location.state || "/");
@@ -84,12 +44,10 @@ const Login = () => {
       }
     } catch (error) {
       console.log(error);
-      // toast is like notification msg
       toast.error("Something went wrong");
     }
   };
 
-  // render the UI
   return (
     <Layout title="Login - Ecommerce App">
       <div className="form-container " style={{ minHeight: "90vh" }}>
@@ -134,9 +92,6 @@ const Login = () => {
             </button>
           </div>
           
-          {/* should disable the button when email or password is empty? */}
-          {/*  disabled={!email || !password} */}
-          {/* not necessarily needed, as the input fields are already marked with required */}
           <button type="submit" className="btn btn-primary">
             LOGIN
           </button>
@@ -146,5 +101,4 @@ const Login = () => {
   );
 };
 
-// used in Login.test.js - import
 export default Login;
