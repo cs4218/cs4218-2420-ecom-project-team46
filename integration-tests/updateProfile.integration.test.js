2dff3b73a7c64b5bf66dee266ad304eade563084
Lam Jiu Fong <e0960521@u.nus.edu>
2025-03-23 19:55:47 +0800

    Add integration test for update user profile api

---
+++ integration-tests/updateProfile.integration.test.js
@@ -0,0 +1,155 @@
+import express from "express";
+import mongoose from "mongoose";
+import User from "../models/userModel";
+import authRoutes from "../routes/authRoute";
+import JWT from "jsonwebtoken";
+import supertest from "supertest";
+import { MongoMemoryServer } from "mongodb-memory-server";
+import { ObjectId } from "mongodb";
+
+const app = express();
+
+app.use(express.json());
+app.use("/api/v1/auth", authRoutes);
+
+let mongoServer;
+
+const user = {
+  _id: new ObjectId(),
+  name: "uesr1",
+  email: "user1@testing.com",
+  password: "password1",
+  phone: "1234567890",
+  address: "bukitpanjang",
+  answer: "blue",
+  role: 0
+}
+
+beforeEach(async () => {
+  process.env.JWT_SECRET = "jwt-secret";
+  mongoServer = await MongoMemoryServer.create();
+
+  if (mongoose.connection.readyState === 0) {
+    await mongoose.connect(mongoServer.getUri());
+  }
+  
+  await User.create(user);
+});
+
+afterEach(async () => {
+  await mongoose.disconnect();
+  await mongoServer.stop();
+});
+
+describe("Update personal information integration test", () => {
+  test("should return status 200 and return updated user data if successful", async () => {
+    const expectedUser = {
+      name: "userr1123",
+      password: "password112313",
+      phone: "123452333",
+      address: "bukitpanjang",
+    }
+    
+    const response = await supertest(app)
+      .put("/api/v1/auth/profile")
+      .set(
+        "Authorization",
+        JWT.sign({ _id: user._id }, process.env.JWT_SECRET)
+      )
+      .send(expectedUser)
+      .expect(200);
+
+    const updatedUser = response.body.updatedUser
+    expect(updatedUser.name).toBe(expectedUser.name);
+    expect(updatedUser.phone).toBe(expectedUser.phone);
+    expect(updatedUser.address).toBe(expectedUser.address);
+  });
+
+  test("should return status 500 if password is too short", async () => {
+    const expectedUser = {
+      name: "userr1123",
+      password: "short",
+      phone: "123452333",
+      address: "bukitpanjang",
+    }
+    
+    await supertest(app)
+    .put("/api/v1/auth/profile")
+    .set(
+      "Authorization",
+      JWT.sign({ _id: user._id }, process.env.JWT_SECRET)
+    )
+    .send(expectedUser)
+    .expect(500);
+  });
+
+  test("should return status 401 if not signed in", async () => {
+    const expectedUser = {
+      name: "userr1123",
+      password: "password112313",
+      phone: "123452333",
+      address: "bukitpanjang",
+    }
+    
+    await supertest(app)
+    .put("/api/v1/auth/profile")
+    .send(expectedUser)
+    .expect(401);
+  });
+
+  test("should return status 401 if JWT is invalid", async () => {
+    const expectedUser = {
+      name: "userr1123",
+      password: "password112313",
+      phone: "123452333",
+      address: "bukitpanjang",
+    }
+    
+    await supertest(app)
+    .put("/api/v1/auth/profile")
+    .set(
+      "Authorization",
+      JWT.sign({ _id: user._id }, "Random secret token")
+    )
+    .send(expectedUser)
+    .expect(401);
+  });
+
+  test("should return status 500 if user is not in database", async () => {
+    const expectedUser = {
+      name: "userr1123",
+      password: "password112313",
+      phone: "123452333",
+      address: "bukitpanjang",
+    }
+    
+    await supertest(app)
+    .put("/api/v1/auth/profile")
+    .set(
+      "Authorization",
+      JWT.sign({ _id: 12345 }, process.env.JWT_SECRET)
+    )
+    .send(expectedUser)
+    .expect(500);
+  });
+
+  test("should return status 500 if error happens in the backend", async () => {
+    await mongoose.disconnect();
+
+    const expectedUser = {
+      name: "userr1123",
+      password: "password112313",
+      phone: "123452333",
+      address: "bukitpanjang",
+    }
+
+    await supertest(app)
+    .put("/api/v1/auth/profile")
+    .set(
+      "Authorization",
+      JWT.sign({ _id: user._id }, process.env.JWT_SECRET)
+    )
+    .send(expectedUser)
+    .expect(500);
+  });
+})
